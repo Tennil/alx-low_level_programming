7b281d8a8e5b064f0a6c91eeaa03cea5e686b69e
tennil <kangethelinnet@gmail.com>
2023-02-21 13:08:58 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /* main -Entry Write a program that prints;
 * Return: 0 (success)
 */
 int main(void);
-{
+{(
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
