67c23a758d0d4543494cea0ffe9f4c54eac1d334
tennil <kangethelinnet@gmail.com>
2023-02-27 02:08:08 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*Main - a function that takes a pointer to an int as parameter
+*@n: pointer to n to be updated
+* Return: 0 (success)
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
