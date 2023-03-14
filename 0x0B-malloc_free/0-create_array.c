272ef7a768578a1e7f48318acfb112c25f574965
tennil <kangethelinnet@gmail.com>
2023-03-14 08:45:31 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+char *create_array(unsigned int size, char c);
+{
+char = *ptrArray;
+unsigned int count;
+
+*ptrArray = malloc(size * size of(char));
+*ptrArray[0] = c
+{
+for count == 0; count < size; count++;
+{
+*ptrArray == c
+}
+}
+return (*ptrArray);
+{
+if (size == 0 || *ptrArray == NULL)
+return (NULL);
+}
+}
